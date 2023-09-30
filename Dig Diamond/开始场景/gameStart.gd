02f31e17a111ef13a31fd70031825cd69ca52e22
秋中午时RousingCN <82033699+RousingCN@users.noreply.github.com>
2023-09-30 14:37:40 +0800

    v 1.0.1
    v 1.0.1

--- Dig Diamond/开始场景/gameStart.gd
+++ Dig Diamond/开始场景/gameStart.gd
@@ -2,7 +2,6 @@ extends TextureRect
 
 signal game_start
 
-
 func _ready():
 	TempData.rowBox=$CenterContainer/VBoxContainer/ButtonCountEnterContainer/Row
 	TempData.colBox=$CenterContainer/VBoxContainer/ButtonCountEnterContainer/Col
@@ -12,11 +11,23 @@ func _ready():
 
 # 开始游戏前初始化游戏设置
 func gameStart():
+	if checkDiamondsCount():
+		hide()
+		emit_signal("game_start")
+
+# 检查输入的数据是否符合规则
+func checkDiamondsCount()-> bool:
 	TempData.row=$CenterContainer/VBoxContainer/ButtonCountEnterContainer/Row.value
 	TempData.col=$CenterContainer/VBoxContainer/ButtonCountEnterContainer/Col.value
 	TempData.diamondCount=$CenterContainer/VBoxContainer/DiamondCountEnterContainer2/DiamondCount.value
-	hide()
-	emit_signal("game_start")
+	
+	if TempData.row * TempData.col< TempData.diamondCount:
+		$CenterContainer/VBoxContainer/ErrorTips.text="钻石的数量不能大于格子的数量"
+		return false
+	else:
+		$CenterContainer/VBoxContainer/ErrorTips.text=""
+		return true
+
 
 # 点击游戏开始按钮
 func _on_start_pressed():
@@ -27,6 +38,25 @@ func _on_start_pressed():
 func _on_exit_pressed():
 	get_tree().quit()
 
+
 # 点击加载游戏按钮
 func _on_load_pressed():
 	pass # Replace with function body.
+
+
+# 修改行数量输入框的值
+func _on_row_value_changed(value):
+	print(1)
+	checkDiamondsCount()
+
+
+# 修改列数量输入框的值
+func _on_col_value_changed(value):
+	print(2)
+	checkDiamondsCount()
+
+
+# 修改钻石数量输入框的值
+func _on_diamond_count_value_changed(value):
+	print(3)
+	checkDiamondsCount()
